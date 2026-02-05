95478d1d7a7be5f289c6951c989acf679dc0ff01
athulyagireesh <athulya@9778#>
2026-02-05 22:25:21 +0530

    gallery vault

---
+++ Galleryvault/app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render,redirect
+import bcrypt
+from .models import *
+
+
+def register(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        password=request.POST['password']
+        cnf_password=request.POST['cnf_password']
+        
+        if password==cnf_password:
+            hashed_password=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+            Users.objects.create(name=name,email=email,password=hashed_password.decode('utf-8'))
+            return redirect(login)      
+    return render(request,'register.html')
+
+
+def login(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+    
+    
+        try:
+            user=Users.objects.get(email=email)
+            print(user)
+
+            if bcrypt.checkpw(
+                password.encode('utf-8'),
+                user.password.encode('utf-8')
+            ):
+                request.session['user']=user.id
+                return redirect(file_upload)
+
+            else:
+                print('Password doesnot match')
+
+        except:
+            print('User doesnot Exists')
+            return redirect(login)
+    return render(request,'login.html')
+
+
+
+def logout(request):
+    request.session.flush()
+    return redirect(login)
+
+
+
+
+def file_upload(request):
+    if 'user' in request.session:
+        user = Users.objects.get(id = request.session['user'])
+        files=Docs.objects.filter(fname=user)
+        if request.method=='POST':
+            title=request.POST['title']
+            doc=request.FILES['file']
+            Docs.objects.create(title=title,file=doc,fname=user)
+        return render(request,'index.html',{'files':files,'user':user})
+    else:
+        return redirect(login)
